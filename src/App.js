26abdeda68b3530d62e9b237781acb49d4b915f4
IURII <lytvyn.139@gmail.com>
2020-09-16 19:50:05 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>To do list:</h1>
+      </header>
+      <Form />
+      <TodoList />
+
+    </div>
+  );
+}
+
+export default App;
